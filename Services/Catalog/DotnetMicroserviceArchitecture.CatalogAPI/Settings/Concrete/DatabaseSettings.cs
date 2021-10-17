ada5370ff0db64af086610d3497afc3d722db6cc
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-10-17 15:21:36 +0300

    Added DatabaseSettings classes

---
+++ Services/Catalog/DotnetMicroserviceArchitecture.CatalogAPI/Settings/Concrete/DatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿using DotnetMicroserviceArchitecture.CatalogAPI.Settings.Abstract;
+
+namespace DotnetMicroserviceArchitecture.CatalogAPI.Settings.Concrete
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string CourseCollectionName { get; set; }
+        public string CategoryCollectionName { get; set; }
+        public string ConnectionStrings { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}

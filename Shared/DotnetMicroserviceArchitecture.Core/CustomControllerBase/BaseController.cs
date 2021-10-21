548386b3d14bcd915c842b650934faeb8965d348
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-10-21 21:36:50 +0300

    last changes and controllers tests

---
+++ Shared/DotnetMicroserviceArchitecture.Core/CustomControllerBase/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using DotnetMicroserviceArchitecture.Core.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetMicroserviceArchitecture.Core.CustomControllerBase
+{
+    public class BaseController : ControllerBase
+    {
+        public IActionResult Result<T>(Response<T> response) =>
+            new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+    }
+}

8bc0e8bb2316d05bce889ab3ac32376930bda707
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-10-22 21:34:12 +0300

    Added Route class

---
+++ Services/Catalog/DotnetMicroserviceArchitecture.CatalogAPI/Constants/Route.cs
@@ -0,0 +1,20 @@
+﻿namespace DotnetMicroserviceArchitecture.CatalogAPI.Constants
+{
+    public class Route
+    {
+        #region Categories Routes Contant
+
+        public const string HTTPGET_CATEGORIES = "Categories";
+        public const string HTTPGET_CATEGORIESBYID = "Categories/{id}";
+
+        #endregion
+
+        #region Courses Routes Contant
+
+        public const string HTTPGET_COURSES = "Courses";
+        public const string HTTPGET_COURSESBYID = "Courses/{id}";
+        public const string HTTPGET_COURSESBYUSERID = "GetAllByUserId/{userId}";
+
+        #endregion
+    }
+}

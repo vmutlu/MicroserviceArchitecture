b1af47cc45e2fe8cefec9ca232c11ae4ac1fbcfb
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-10-23 13:45:10 +0300

    UserControllerClasses added

---
+++ IdentityServer/DotnetMicroserviceArchitecture.IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using DotnetMicroserviceArchitecture.Core.Dtos;
+using DotnetMicroserviceArchitecture.IdentityServer.DTOs;
+using DotnetMicroserviceArchitecture.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DotnetMicroserviceArchitecture.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDTO signUpDTO)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDTO.UserName,
+                Email = signUpDTO.Email,
+                City = signUpDTO.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signUpDTO.Password).ConfigureAwait(false);
+            if (!result.Succeeded)
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), (int)HttpStatusCode.NotFound));
+
+            return Ok(Response<NoContent>.Success((int)HttpStatusCode.NoContent));
+        }
+    }
+}

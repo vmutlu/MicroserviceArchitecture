f862bd3780497b01a5e69670aeef56df9570139e
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-10-24 18:25:37 +0300

    created dtos classes

--- IdentityServer/DotnetMicroserviceArchitecture.IdentityServer/DTOs/SignUpDTO.cs
+++ IdentityServer/DotnetMicroserviceArchitecture.IdentityServer/DTOs/SignUpDTO.cs
@@ -1,10 +1,19 @@
-﻿namespace DotnetMicroserviceArchitecture.IdentityServer.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotnetMicroserviceArchitecture.IdentityServer.DTOs
 {
     public class SignUpDTO
     {
+        [Required]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [Required]
         public string UserName { get; set; }
+
+        [Required]
         public string City { get; set; }
     }
 }

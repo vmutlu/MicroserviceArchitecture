e647e80376ab9f78068a1fcf31e8bfedeb7727c2
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-10-30 11:38:40 +0300

    Basket dto classes created

---
+++ Services/Basket/DotnetMicroserviceArchitecture.BasketAPI/DTOs/BasketItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DotnetMicroserviceArchitecture.BasketAPI.DTOs
+{
+    public class BasketItemDTO
+    {
+        public string CourseId { get; set; }
+        public int Quantity { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+    }
+}

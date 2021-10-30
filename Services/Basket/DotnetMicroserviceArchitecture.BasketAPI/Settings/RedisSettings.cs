cd8925e6f5976583e1d6eea857d4ee50e0d3536e
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-10-30 11:39:38 +0300

    redis host and port properties added

---
+++ Services/Basket/DotnetMicroserviceArchitecture.BasketAPI/Settings/RedisSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetMicroserviceArchitecture.BasketAPI.Settings
+{
+    public class RedisSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}

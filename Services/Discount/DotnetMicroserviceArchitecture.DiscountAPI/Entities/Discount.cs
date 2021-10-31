96232734f2967c5ab73472c7cff5d6912052515b
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-10-31 16:07:59 +0300

    Discount poco class added

---
+++ Services/Discount/DotnetMicroserviceArchitecture.DiscountAPI/Entities/Discount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotnetMicroserviceArchitecture.DiscountAPI.Entities
+{
+    //postgresql db de best practive olarak tablo isimleri küçük harfle başladığı için
+    [Dapper.Contrib.Extensions.Table("discount")]
+    public class Discount
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int Rate { get; set; }
+        public string Code { get; set; }
+        public DateTime InsertedDate { get; set; } = DateTime.UtcNow.Date;
+    }
+}

7714f1fd84c8c670bf44f0eb44a88829dd00906a
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-10-31 16:08:57 +0300

    last changes

---
+++ Services/Discount/DotnetMicroserviceArchitecture.DiscountAPI/Controllers/DiscountsController.cs
@@ -0,0 +1,65 @@
+﻿using DotnetMicroserviceArchitecture.Core.CustomControllerBase;
+using DotnetMicroserviceArchitecture.Core.Services.Abstract;
+using DotnetMicroserviceArchitecture.DiscountAPI.Entities;
+using DotnetMicroserviceArchitecture.DiscountAPI.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DotnetMicroserviceArchitecture.DiscountAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : BaseController
+    {
+        private readonly IIdentityService _identityService;
+        private readonly IDiscountService _discountService;
+
+        public DiscountsController(IIdentityService identityService, IDiscountService discountService)
+        {
+            _identityService = identityService;
+            _discountService = discountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Result(await _discountService.GetAll().ConfigureAwait(false));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var discount = await _discountService.GetById(id).ConfigureAwait(false);
+
+            return Result(discount);
+        }
+
+        [HttpGet("GetByCode/{code}")]
+        public async Task<IActionResult> GetByCode(string code)
+        {
+            var userId = _identityService.GetUserId;
+
+            var discount = await _discountService.GetByCodeAndUserId(code, userId).ConfigureAwait(false);
+
+            return Result(discount);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Discount discount)
+        {
+            return Result(await _discountService.Add(discount).ConfigureAwait(false));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Discount discount)
+        {
+            return Result(await _discountService.Update(discount).ConfigureAwait(false));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Result(await _discountService.Delete(id).ConfigureAwait(false));
+        }
+    }
+}

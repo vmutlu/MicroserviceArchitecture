a51223828737f33cc76dcec07eb5840425b4ec38
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-03 21:07:21 +0300

    base classes edited

--- Services/Order/DotnetMicroserviceArchitecture.Order.Core/Entities/BaseEntity.cs
+++ Services/Order/DotnetMicroserviceArchitecture.Order.Core/Entities/BaseEntity.cs
@@ -1,6 +1,61 @@
-﻿namespace DotnetMicroserviceArchitecture.Order.Core.Entities
+﻿using System;
+
+namespace DotnetMicroserviceArchitecture.Order.Core.Entities
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
+        private int? _requestedHashCode;
+
+        public virtual int Id { get; set; }
+
+        public bool IsTransient() => Id == default(Int32);
+
+        public override int GetHashCode()
+        {
+            if (!IsTransient())
+            {
+                if (!_requestedHashCode.HasValue)
+                    _requestedHashCode = Id.GetHashCode() ^ 31; // XOR for random distribution (http://blogs.msdn.com/b/ericlippert/archive/2011/02/28/guidelines-and-rules-for-gethashcode.aspx)
+
+                return _requestedHashCode.Value;
+            }
+            else
+                return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is BaseEntity))
+                return false;
+
+            if (Object.ReferenceEquals(this, obj))
+                return true;
+
+            if (this.GetType() != obj.GetType())
+                return false;
+
+            BaseEntity item = (BaseEntity)obj;
+
+            if (item.IsTransient() || this.IsTransient())
+                return false;
+            else
+                return item.Id == Id;
+        }
+
+        public static bool operator ==(BaseEntity left, BaseEntity right)
+        {
+            if (Object.Equals(left, null))
+                return (Object.Equals(right, null)) ? true : false;
+            else
+                return left.Equals(right);
+        }
+
+        public static bool operator !=(BaseEntity left, BaseEntity right)
+        {
+            if (Object.Equals(left, null))
+                return (Object.Equals(right, null)) ? true : false;
+            else
+                return left.Equals(right);
+        }
     }
 }
--- Services/Order/DotnetMicroserviceArchitecture.Order.Core/Entities/BaseValueObject.cs
+++ Services/Order/DotnetMicroserviceArchitecture.Order.Core/Entities/BaseValueObject.cs
@@ -1,6 +1,39 @@
-﻿namespace DotnetMicroserviceArchitecture.Order.Core.Entities
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DotnetMicroserviceArchitecture.Order.Core.Entities
 {
-    public class BaseValueObject
+    public abstract class BaseValueObject
     {
+        protected static bool EqualOperator(BaseValueObject left, BaseValueObject right)
+        {
+            if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+                return false;
+
+            return ReferenceEquals(left, null) || left.Equals(right);
+        }
+
+        protected static bool NotEqualOperator(BaseValueObject left, BaseValueObject right) => !(EqualOperator(left, right));
+
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != GetType())
+                return false;
+
+            var other = (BaseValueObject)obj;
+
+            return this.GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+             .Select(x => x != null ? x.GetHashCode() : 0)
+             .Aggregate((x, y) => x ^ y);
+        }
+
+        public BaseValueObject GetCopy() => this.MemberwiseClone() as BaseValueObject;
     }
 }

7b5215d03078f9aa1fa6e649dd2e5f39049e0f19
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-03 21:42:58 +0300

    Order entity classes added

---
+++ Services/Order/DotnetMicroserviceArchitecture.Order.Domain/Aggregate/Order.cs
@@ -0,0 +1,36 @@
+﻿using DotnetMicroserviceArchitecture.Order.Core.Abstract;
+using DotnetMicroserviceArchitecture.Order.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotnetMicroserviceArchitecture.Order.Domain.Aggregate
+{
+    public class Order : BaseEntity, IAggregateRoot
+    {
+        public DateTime InsertedDate { get; private set; }
+        public Adress Adress { get; private set; }
+        public string BuyerId { get; private set; }
+
+        private readonly List<OrderItem> _orderItems; //okuma yazma işlemi property değil de buşekilde field üzerinde gerçekleştirilirse backing field olarak adlandırılır.
+
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public Order(string buyerId, Adress adress)
+        {
+            _orderItems = new();
+            InsertedDate = DateTime.Now;
+            BuyerId = buyerId;
+            Adress = adress;
+        }
+
+        public void AddOrderItem(string orderId, string name, string uRL, decimal price)
+        {
+            var exits = _orderItems.Any(x => x.OrderId == orderId);
+            if (!exits)
+                _orderItems.Add(new OrderItem(orderId, name, uRL, price));
+        }
+
+        public decimal TotalPrice() => _orderItems.Sum(x => x.Price);
+    }
+}

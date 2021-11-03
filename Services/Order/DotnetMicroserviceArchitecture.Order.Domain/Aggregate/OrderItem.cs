4c7fa9e6c8b26d418a6e7bb8ce163c26d7045a60
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-03 21:43:11 +0300

    OrderItem entity classes added

---
+++ Services/Order/DotnetMicroserviceArchitecture.Order.Domain/Aggregate/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using DotnetMicroserviceArchitecture.Order.Core.Entities;
+
+namespace DotnetMicroserviceArchitecture.Order.Domain.Aggregate
+{
+    public class OrderItem : BaseEntity
+    {
+        public string OrderId { get; private set; }
+        public string Name { get; private set; }
+        public string URL { get; private set; }
+        public decimal Price { get; private set; }
+
+        public OrderItem(string orderId, string name, string uRL, decimal price)
+        {
+            OrderId = orderId;
+            Name = name;
+            URL = uRL;
+            Price = price;
+        }
+
+        public void UpdateOrderItem(string name, string uRL, decimal price)
+        {
+            Name = name;
+            URL = uRL;
+            Price = price;
+        }
+    }
+}

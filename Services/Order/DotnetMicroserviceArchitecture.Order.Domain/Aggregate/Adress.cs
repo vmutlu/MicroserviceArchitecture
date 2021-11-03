b7a79eb56eb2a7d88494a80354c24635ad814132
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-03 21:42:48 +0300

    Address value object added

---
+++ Services/Order/DotnetMicroserviceArchitecture.Order.Domain/Aggregate/Adress.cs
@@ -0,0 +1,31 @@
+﻿using DotnetMicroserviceArchitecture.Order.Core.Entities;
+using System.Collections.Generic;
+
+namespace DotnetMicroserviceArchitecture.Order.Domain.Aggregate
+{
+    public class Adress : BaseValueObject
+    {
+        public string City { get; private set; }
+        public string Town { get; private set; }
+        public string Street { get; private set; }
+        public string ZipCode { get; private set; }
+        public string Line { get; private set; }
+        public Adress(string city, string town, string street, string zipCode, string line)
+        {
+            City = city;
+            Town = town;
+            Street = street;
+            ZipCode = zipCode;
+            Line = line;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return City;
+            yield return Town;
+            yield return Street;
+            yield return ZipCode;
+            yield return Line;
+        }
+    }
+}

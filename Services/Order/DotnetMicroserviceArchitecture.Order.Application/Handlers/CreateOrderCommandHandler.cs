02aaf219797b26e17d24d0a9b675e82b93561178
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-05 22:58:33 +0300

    handler class created

---
+++ Services/Order/DotnetMicroserviceArchitecture.Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using DotnetMicroserviceArchitecture.Core.Dtos;
+using DotnetMicroserviceArchitecture.Order.Application.Commands;
+using DotnetMicroserviceArchitecture.Order.Application.DTOs;
+using DotnetMicroserviceArchitecture.Order.Domain.Aggregate;
+using DotnetMicroserviceArchitecture.Order.Infrastructure.Context;
+using MediatR;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotnetMicroserviceArchitecture.Order.Application.Handlers
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Response<InsertedOrderDTO>>
+    {
+        private readonly OrderContext _orderContext;
+
+        public CreateOrderCommandHandler(OrderContext orderContext)
+        {
+            _orderContext = orderContext;
+        }
+        public async Task<Response<InsertedOrderDTO>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            Adress address = new(request?.AdressDTO?.City, request?.AdressDTO?.Town, request?.AdressDTO?.Street, request?.AdressDTO?.ZipCode, request?.AdressDTO?.Line);
+
+            DotnetMicroserviceArchitecture.Order.Domain.Aggregate.Order order = new(request?.BuyerId, address);
+
+            request.OrderItems?.ForEach(x =>
+            {
+                order.AddOrderItem(x.OrderId, x.Name, x.URL, x.Price);
+            });
+
+            await _orderContext.Orders.AddAsync(order).ConfigureAwait(false);
+
+            await _orderContext.SaveChangesAsync().ConfigureAwait(false);
+
+            return Response<InsertedOrderDTO>.Success(new InsertedOrderDTO() { OrderId = order.Id }, HttpStatusCode.OK.GetHashCode());
+        }
+    }
+}
---
+++ Services/Order/DotnetMicroserviceArchitecture.Order.Application/Handlers/GetOrderByUserIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using DotnetMicroserviceArchitecture.Core.Dtos;
+using DotnetMicroserviceArchitecture.Order.Application.DTOs;
+using DotnetMicroserviceArchitecture.Order.Application.Mapping;
+using DotnetMicroserviceArchitecture.Order.Application.Querys;
+using DotnetMicroserviceArchitecture.Order.Infrastructure.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotnetMicroserviceArchitecture.Order.Application.Handlers
+{
+    public class GetOrderByUserIdQueryHandler : IRequestHandler<GetOrderByUserIdQuery, Response<List<OrderDTO>>>
+    {
+        private readonly OrderContext _orderContext;
+
+        public GetOrderByUserIdQueryHandler(OrderContext orderContext)
+        {
+            _orderContext = orderContext;
+        }
+
+        public async Task<Response<List<OrderDTO>>> Handle(GetOrderByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderContext.Orders.Include(x => x.OrderItems).Where(x => x.BuyerId == request.UserId).ToListAsync().ConfigureAwait(false);
+
+            if (orders.Any())
+                return Response<List<OrderDTO>>.Success(new List<OrderDTO>(), HttpStatusCode.OK.GetHashCode());
+
+            var mappedOrderDTO = ObjectMapper.Mapper.Map<List<OrderDTO>>(orders);
+
+            return Response<List<OrderDTO>>.Success(mappedOrderDTO, HttpStatusCode.OK.GetHashCode());
+        }
+    }
+}

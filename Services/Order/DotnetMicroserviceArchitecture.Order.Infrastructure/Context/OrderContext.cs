4329e135fff4fcfd490f7073b20cc0fe2df7fbea
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-05 22:58:11 +0300

    context classes added

---
+++ Services/Order/DotnetMicroserviceArchitecture.Order.Infrastructure/Context/OrderContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotnetMicroserviceArchitecture.Order.Infrastructure.Context
+{
+    public class OrderContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "ORDER";
+
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DotnetMicroserviceArchitecture.Order.Domain.Aggregate.Order> Orders { get; set; }
+        public DbSet<DotnetMicroserviceArchitecture.Order.Domain.Aggregate.OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DotnetMicroserviceArchitecture.Order.Domain.Aggregate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<DotnetMicroserviceArchitecture.Order.Domain.Aggregate.OrderItem>().ToTable("OrderItems", DEFAULT_SCHEMA);
+
+            modelBuilder.Entity<DotnetMicroserviceArchitecture.Order.Domain.Aggregate.OrderItem>().Property(X => X.Price).HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<DotnetMicroserviceArchitecture.Order.Domain.Aggregate.Order>().OwnsOne(x => x.Adress).WithOwner();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

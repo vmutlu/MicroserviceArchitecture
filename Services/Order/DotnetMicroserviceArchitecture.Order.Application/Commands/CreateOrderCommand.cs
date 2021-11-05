5a27da97506254a70580ad4b86c8a6fd014ff4c7
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-05 22:58:47 +0300

    command classes created

---
+++ Services/Order/DotnetMicroserviceArchitecture.Order.Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using DotnetMicroserviceArchitecture.Core.Dtos;
+using DotnetMicroserviceArchitecture.Order.Application.DTOs;
+using MediatR;
+using System.Collections.Generic;
+
+namespace DotnetMicroserviceArchitecture.Order.Application.Commands
+{
+    public class CreateOrderCommand : IRequest<Response<InsertedOrderDTO>>
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItemDTO> OrderItems { get; set; }
+        public AdressDTO AdressDTO { get; set; }
+    }
+}

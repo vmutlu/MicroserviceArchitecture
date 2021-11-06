f9491c5d6c4bba38aa1abd7955b63116b72ae5c9
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-06 20:40:29 +0300

    example payment endpoind added

---
+++ Services/Payment/DotnetMicroserviceArchitecture.PaymentAPI/Controllers/PaymentsController.cs
@@ -0,0 +1,15 @@
+﻿using DotnetMicroserviceArchitecture.Core.CustomControllerBase;
+using DotnetMicroserviceArchitecture.Core.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace DotnetMicroserviceArchitecture.PaymentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : BaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment() => Result<NoContent>(Response<NoContent>.Success(HttpStatusCode.OK.GetHashCode()));
+    }
+}

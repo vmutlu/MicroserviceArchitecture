8b5f3b41a8c1505207d2be21b9f3c32e37bbb71b
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-06 20:16:33 +0300

    db created

---
+++ Services/Order/DotnetMicroserviceArchitecture.Order.Infrastructure/Migrations/20211106155250_order_initialize_migration.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotnetMicroserviceArchitecture.Order.Infrastructure.Migrations
+{
+    public partial class order_initialize_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ORDER");
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                schema: "ORDER",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InsertedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Adress_City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adress_Town = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adress_Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adress_ZipCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adress_Line = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BuyerId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                schema: "ORDER",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    URL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "ORDER",
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId",
+                schema: "ORDER",
+                table: "OrderItems",
+                column: "OrderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItems",
+                schema: "ORDER");
+
+            migrationBuilder.DropTable(
+                name: "Orders",
+                schema: "ORDER");
+        }
+    }
+}

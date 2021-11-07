2b3fe23872c107da6d8c2a50d67148a0a4e80d62
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-07 13:09:15 +0300

    default catalog datas added

--- Services/Catalog/DotnetMicroserviceArchitecture.CatalogAPI/Program.cs
+++ Services/Catalog/DotnetMicroserviceArchitecture.CatalogAPI/Program.cs
@@ -1,11 +1,8 @@
+using DotnetMicroserviceArchitecture.CatalogAPI.Services.Abstract;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace DotnetMicroserviceArchitecture.CatalogAPI
 {
@@ -13,7 +10,33 @@ namespace DotnetMicroserviceArchitecture.CatalogAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var catalogService = serviceProvider.GetRequiredService<ICategoryService>();
+
+                if (!catalogService.GetAllAsync().Result.Data.Any())
+                {
+                    catalogService.AddAsync(new Dtos.CategoryDTO()
+                    {
+                        Name = "Example Category - 1"
+                    }).Wait();
+
+                    catalogService.AddAsync(new Dtos.CategoryDTO()
+                    {
+                        Name = "Example Category - 2"
+                    }).Wait();
+
+                    catalogService.AddAsync(new Dtos.CategoryDTO()
+                    {
+                        Name = "Example Category - 3"
+                    }).Wait();
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

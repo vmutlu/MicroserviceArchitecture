2cb8fa3cbb975e50155e20e841c1bd4fd23cf9f4
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-09 20:48:50 +0300

    Option pattern applied

---
+++ UI/DotnetMicroserviceArchitecture.UI/ApiSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetMicroserviceArchitecture.UI
+{
+    public class ApiSettings
+    {
+        public string BaseURL { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
--- UI/DotnetMicroserviceArchitecture.UI/Startup.cs
+++ UI/DotnetMicroserviceArchitecture.UI/Startup.cs
@@ -3,29 +3,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DotnetMicroserviceArchitecture.UI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => (Configuration) = (configuration);
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ApiSettings>(Configuration.GetSection("ApiSettings"));
+
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())

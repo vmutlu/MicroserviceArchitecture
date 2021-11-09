ebe012b7867ecdb231a06c097b46237b3a1ccaee
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-09 20:43:35 +0300

    SignInModel classes added

---
+++ UI/DotnetMicroserviceArchitecture.UI/Models/SignInModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotnetMicroserviceArchitecture.UI.Models
+{
+    public class SignInModel
+    {
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool IsRemember { get; set; }
+    }
+}

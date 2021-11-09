ba35238258e3dcc900426db25bffe9c1575d9114
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-09 20:43:26 +0300

    IIdentityService interface created

---
+++ UI/DotnetMicroserviceArchitecture.UI/Services/Abstract/IIdentityService.cs
@@ -0,0 +1,14 @@
+﻿using DotnetMicroserviceArchitecture.Core.Dtos;
+using DotnetMicroserviceArchitecture.UI.Models;
+using IdentityModel.Client;
+using System.Threading.Tasks;
+
+namespace DotnetMicroserviceArchitecture.UI.Services.Abstract
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SignInModel signInModel);
+        Task<TokenResponse> GetAccessTokenByRefleshToken();
+        Task RemoveRefleshToken();
+    }
+}

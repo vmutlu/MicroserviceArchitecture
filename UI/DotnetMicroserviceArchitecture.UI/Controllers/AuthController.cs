ff72dce6365e0d53099ac05b9bc3d55cbcf58cff
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-11 21:35:11 +0300

    AuthController created

---
+++ UI/DotnetMicroserviceArchitecture.UI/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using DotnetMicroserviceArchitecture.UI.Models;
+using DotnetMicroserviceArchitecture.UI.Services.Abstract;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace DotnetMicroserviceArchitecture.UI.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInModel signInModel)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var response = await _identityService.SignIn(signInModel).ConfigureAwait(false);
+
+            if (!response.IsSuccess)
+            {
+                response.Errors.ForEach(x =>
+                {
+                    ModelState.AddModelError(String.Empty, x);
+                });
+
+                return View();
+            }
+
+            return RedirectToAction(nameof(Index), "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await _identityService.RemoveRefleshToken().ConfigureAwait(false);
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}

4f4df83208907fa75db067575b1940aecad4f9be
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-15 21:10:14 +0300

    Added interface and view models for catalog microservice communication. Inter-microservice communication will be investigated and implemented

---
+++ UI/DotnetMicroserviceArchitecture.UI/Services/Abstract/ICatalogService.cs
@@ -0,0 +1,16 @@
+﻿using DotnetMicroserviceArchitecture.UI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotnetMicroserviceArchitecture.UI.Services.Abstract
+{
+    public interface ICatalogService
+    {
+        Task<List<CourseView>> GetAllAsync();
+        Task<List<CourseView>> GetAllByUserIdAsync(string userId);
+        Task<bool> DeleteAsync(string catalogId);
+        Task<bool> AddAsync(CourseCreateContract courseCreateContract);
+        Task<bool> UpdateAsync(CourseUpdateContract courseUpdateContract);
+        Task<CourseView> GetByCatalogIdAsync(string catalogId);
+    }
+}

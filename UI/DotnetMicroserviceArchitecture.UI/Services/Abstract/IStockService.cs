32d5fb49fd3df6b2a9f90656325d0bde60f6fbc5
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-20 14:33:23 +0300

    stockservice classes added and stockservice for communicated

---
+++ UI/DotnetMicroserviceArchitecture.UI/Services/Abstract/IStockService.cs
@@ -0,0 +1,12 @@
+﻿using DotnetMicroserviceArchitecture.UI.Models;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace DotnetMicroserviceArchitecture.UI.Services.Abstract
+{
+    public interface IStockService
+    {
+        Task<StockView> UploadImageAsync(IFormFile formFile);
+        Task<bool> DeleteAsync(string imageURL);
+    }
+}

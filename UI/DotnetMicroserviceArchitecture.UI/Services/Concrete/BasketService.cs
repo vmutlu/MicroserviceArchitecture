7fe6cefb411c97e34282c5743c6ab0c810eb5c5c
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-20 22:08:33 +0300

    BasketService Methods writed

--- UI/DotnetMicroserviceArchitecture.UI/Services/Concrete/BasketService.cs
+++ UI/DotnetMicroserviceArchitecture.UI/Services/Concrete/BasketService.cs
@@ -1,7 +1,10 @@
-﻿using DotnetMicroserviceArchitecture.UI.Models;
+﻿using DotnetMicroserviceArchitecture.Core.Dtos;
+using DotnetMicroserviceArchitecture.UI.Models;
 using DotnetMicroserviceArchitecture.UI.Services.Abstract;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace DotnetMicroserviceArchitecture.UI.Services.Concrete
 {
@@ -13,39 +16,80 @@ namespace DotnetMicroserviceArchitecture.UI.Services.Concrete
         {
             _httpClient = httpClient;
         }
-        public Task AddBasketItemAsync(BasketItemView basketItemView)
+        public async Task AddBasketItemAsync(BasketItemView basketItemView)
+        {
+            var exitsBasket = await GetAsync().ConfigureAwait(false);
+
+            if (exitsBasket is not null)
+                if (!exitsBasket.BasketItems.Any(x => x.CourseId == basketItemView.CourseId))
+                    exitsBasket.BasketItems.Add(basketItemView);
+
+                else
+                {
+                    exitsBasket = new BasketView();
+                    exitsBasket.BasketItems.Add(basketItemView);
+                }
+
+            await AddOrUpdateAsync(exitsBasket).ConfigureAwait(false);
+        }
+
+        public async Task<bool> AddOrUpdateAsync(BasketView basketView)
+        {
+            var response = await _httpClient.PostAsJsonAsync<BasketView>("baskets/baskets", basketView).ConfigureAwait(false);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> ApplyDiscountAsync(string discountCode)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<bool> AddOrUpdateAsync(BasketView basketView)
+        public async Task<bool> CancelApplyDiscountAsync()
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<bool> ApplyDiscountAsync(string discountCode)
+        public async Task<bool> DeleteAsync()
         {
-            throw new System.NotImplementedException();
+            var response = await _httpClient.DeleteAsync("baskets/baskets").ConfigureAwait(false);
+
+            return response.IsSuccessStatusCode;
         }
 
-        public Task<bool> CancelApplyDiscountAsync()
+        public async Task<bool> DeleteBasketItemAsync(string courseId)
         {
-            throw new System.NotImplementedException();
+            var response = await GetAsync().ConfigureAwait(false);
+
+            if (response is null)
+                return false;
+
+            var deletedItem = response.BasketItems.FirstOrDefault(x => x.CourseId == courseId);
+
+            if (deletedItem is null)
+                return false;
+
+            var deleteBasket = response.BasketItems.Remove(deletedItem);
+
+            if (deleteBasket is not true)
+                return false;
+
+            if (!response.BasketItems.Any())
+                response.DiscountCode = null;
+
+            return await AddOrUpdateAsync(response).ConfigureAwait(false);
         }
 
-        public Task<bool> DeleteAsync()
+        public async Task<BasketView> GetAsync()
         {
-            throw new System.NotImplementedException();
-        }
+            var response = await _httpClient.GetAsync("baskets/baskets").ConfigureAwait(false);
 
-        public Task<bool> DeleteBasketItemAsync(string courseId)
-        {
-            throw new System.NotImplementedException();
-        }
+            if (!response.IsSuccessStatusCode)
+                return null;
 
-        public Task<BasketView> GetAsync()
-        {
-            throw new System.NotImplementedException();
+            var baskets = await response.Content.ReadFromJsonAsync<Response<BasketView>>().ConfigureAwait(false);
+
+            return baskets.Data;
         }
     }
 }

14e2b06986419fdc85e94609553f4e036b134077
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-20 21:41:32 +0300

    startup.cs class for extension metod writed

---
+++ UI/DotnetMicroserviceArchitecture.UI/Extensions/StartupExtensions.cs
@@ -0,0 +1,72 @@
+﻿using DotnetMicroserviceArchitecture.Core.Services.Concrete;
+using DotnetMicroserviceArchitecture.UI.Handler;
+using DotnetMicroserviceArchitecture.UI.Helpers;
+using DotnetMicroserviceArchitecture.UI.Services.Abstract;
+using DotnetMicroserviceArchitecture.UI.Services.Concrete;
+using DotnetMicroserviceArchitecture.UI.Settings;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DotnetMicroserviceArchitecture.UI.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddServices(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddHttpClient<Services.Abstract.IIdentityService, Services.Concrete.IdentityService>();
+
+            var apiSettings = Configuration.GetSection("ApiSettings").Get<ApiSettings>();
+
+            //category service implementation
+            services.AddHttpClient<ICatalogService, CatalogService>(options =>
+            {
+                options.BaseAddress = new Uri($"{ apiSettings.GatewayURL }/{apiSettings.Catalog.Path}");
+            }).AddHttpMessageHandler<TokenHandler>();
+
+            //stock service implementation
+            services.AddHttpClient<IStockService, StockService>(options =>
+            {
+                options.BaseAddress = new Uri($"{ apiSettings.GatewayURL }/{apiSettings.Stock.Path}");
+            }).AddHttpMessageHandler<TokenHandler>();
+
+            //basket service implementation
+            services.AddHttpClient<IBasketService, BasketService>(options =>
+            {
+                options.BaseAddress = new Uri($"{ apiSettings.GatewayURL }/{apiSettings.Basket.Path}");
+            }).AddHttpMessageHandler<TokenHandler>();
+
+            services.AddHttpClient<ITokenService, TokenService>();
+
+            services.AddSingleton<PhotoURLEditHelper>();
+
+            services.AddAccessTokenManagement();
+
+            services.AddScoped<ResourceOwnerTokenHandler>();
+
+            services.AddScoped<TokenHandler>();
+
+            services.AddScoped<Core.Services.Abstract.IIdentityService, Core.Services.Concrete.IdentityService>();
+
+            services.AddHttpContextAccessor();
+
+            services.Configure<ClientSettings>(Configuration.GetSection("ClientSettings"));
+
+            services.Configure<ApiSettings>(Configuration.GetSection("ApiSettings"));
+
+            services.AddHttpClient<IUserService, UserService>(options =>
+            {
+                options.BaseAddress = new Uri(apiSettings.IdentityURL);
+            }).AddHttpMessageHandler<ResourceOwnerTokenHandler>();//her istekte headere token bilgisi eklemesi için
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.LoginPath = "/Auth/SignIn";
+                options.ExpireTimeSpan = TimeSpan.FromDays(60);
+                options.SlidingExpiration = true; options.Cookie.Name = "microserviceprojectcookie";
+            });
+
+        }
+    }
+}
--- UI/DotnetMicroserviceArchitecture.UI/Startup.cs
+++ UI/DotnetMicroserviceArchitecture.UI/Startup.cs
@@ -1,3 +1,4 @@
+using DotnetMicroserviceArchitecture.UI.Extensions;
 using DotnetMicroserviceArchitecture.UI.Handler;
 using DotnetMicroserviceArchitecture.UI.Helpers;
 using DotnetMicroserviceArchitecture.UI.Services.Abstract;
@@ -21,51 +22,7 @@ namespace DotnetMicroserviceArchitecture.UI
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient<IIdentityService, IdentityService>();
-
-            var apiSettings = Configuration.GetSection("ApiSettings").Get<ApiSettings>();
-
-            //category service implementation
-            services.AddHttpClient<ICatalogService, CatalogService>(options =>
-            {
-                options.BaseAddress = new Uri($"{ apiSettings.GatewayURL }/{apiSettings.Catalog.Path}");
-            }).AddHttpMessageHandler<TokenHandler>();
-
-            //stock service implementation
-            services.AddHttpClient<IStockService, StockService>(options =>
-            {
-                options.BaseAddress = new Uri($"{ apiSettings.GatewayURL }/{apiSettings.Stock.Path}");
-            }).AddHttpMessageHandler<TokenHandler>();
-
-            services.AddHttpClient<ITokenService, TokenService>();
-
-            services.AddSingleton<PhotoURLEditHelper>();
-
-            services.AddAccessTokenManagement();
-
-            services.AddScoped<ResourceOwnerTokenHandler>();
-
-            services.AddScoped<TokenHandler>();
-
-            services.AddScoped<DotnetMicroserviceArchitecture.Core.Services.Abstract.IIdentityService, DotnetMicroserviceArchitecture.Core.Services.Concrete.IdentityService>();
-
-            services.AddHttpContextAccessor();
-
-            services.Configure<ClientSettings>(Configuration.GetSection("ClientSettings"));
-
-            services.Configure<ApiSettings>(Configuration.GetSection("ApiSettings"));
-
-            services.AddHttpClient<IUserService, UserService>(options =>
-            {
-                options.BaseAddress = new Uri(apiSettings.IdentityURL);
-            }).AddHttpMessageHandler<ResourceOwnerTokenHandler>();//her istekte headere token bilgisi eklemesi için
-
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
-            {
-                options.LoginPath = "/Auth/SignIn";
-                options.ExpireTimeSpan = TimeSpan.FromDays(60);
-                options.SlidingExpiration = true; options.Cookie.Name = "microserviceprojectcookie";
-            });
+            services.AddServices(Configuration);
 
             services.AddControllersWithViews();
         }

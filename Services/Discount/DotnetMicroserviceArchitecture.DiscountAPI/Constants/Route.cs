8747e6eb9c57e0bcbd8ea57f2485f70831ae19d1
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-21 15:32:04 +0300

    discount endpointleri edited

---
+++ Services/Discount/DotnetMicroserviceArchitecture.DiscountAPI/Constants/Route.cs
@@ -0,0 +1,13 @@
+﻿namespace DotnetMicroserviceArchitecture.DiscountAPI.Constants
+{
+    public class Route
+    {
+        #region DiscountController Routes Contant
+
+        public const string HTTPGETORPOST_DISCOUNTS = "discounts";
+        public const string HTTPGET_DISCOUNTS = "discounts/{id}";
+        public const string HTTPGETBYCODE_DISCOUNTS = "discounts/GetByCode/{code}";
+
+        #endregion
+    }
+}

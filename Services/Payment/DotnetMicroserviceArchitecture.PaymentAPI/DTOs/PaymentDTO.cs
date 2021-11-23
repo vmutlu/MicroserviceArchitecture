79fbcecc28e6eefb4e6bc56163a577d9685c2fb7
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-23 19:29:30 +0300

    PaymentDTO created

---
+++ Services/Payment/DotnetMicroserviceArchitecture.PaymentAPI/DTOs/PaymentDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace DotnetMicroserviceArchitecture.PaymentAPI.DTOs
+{
+    public class PaymentDTO
+    {
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string Cvv { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}

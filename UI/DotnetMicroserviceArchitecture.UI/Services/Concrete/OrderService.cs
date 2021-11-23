0b87955115a8c6ea07d13bcda47839244ff92882
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-23 19:48:44 +0300

    orderservice classes added

---
+++ UI/DotnetMicroserviceArchitecture.UI/Services/Concrete/OrderService.cs
@@ -0,0 +1,15 @@
+﻿using DotnetMicroserviceArchitecture.UI.Services.Abstract;
+using System.Net.Http;
+
+namespace DotnetMicroserviceArchitecture.UI.Services.Concrete
+{
+    public class OrderService: IOrderService
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrderService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}

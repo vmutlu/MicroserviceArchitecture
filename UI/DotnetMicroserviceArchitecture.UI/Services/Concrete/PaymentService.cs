8562a92b1470bb1ebdf30c578fd8e5b912eb2e02
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-23 19:36:16 +0300

    PaymentService writed

--- UI/DotnetMicroserviceArchitecture.UI/Services/Concrete/PaymentService.cs
+++ UI/DotnetMicroserviceArchitecture.UI/Services/Concrete/PaymentService.cs
@@ -1,14 +1,24 @@
 ﻿using DotnetMicroserviceArchitecture.UI.Models;
 using DotnetMicroserviceArchitecture.UI.Services.Abstract;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace DotnetMicroserviceArchitecture.UI.Services.Concrete
 {
     public class PaymentService : IPaymentService
     {
-        public Task<bool> ReceivePayment(PaymentInfoRequest paymentInfoRequest)
+        private readonly HttpClient _httpClient;
+
+        public PaymentService(HttpClient httpClient)
         {
-            throw new System.NotImplementedException();
+            _httpClient = httpClient;
+        }
+        public async Task<bool> ReceivePayment(PaymentInfoRequest paymentInfoRequest)
+        {
+            var response = await _httpClient.PostAsJsonAsync<PaymentInfoRequest>("payments/payments", paymentInfoRequest).ConfigureAwait(false);
+
+            return response.IsSuccessStatusCode;
         }
     }
 }

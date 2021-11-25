0dbfb5eee5f41a7765e9e02500758642c1efdb55
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-25 19:58:00 +0300

    ordermicroservice for rabbitmq using.

---
+++ Shared/DotnetMicroserviceArchitecture.Core/Commands/CreateOrderMessageCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace DotnetMicroserviceArchitecture.Core.Commands
+{
+    public class CreateOrderMessageCommand
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItemCommand> OrderItems { get; set; } 
+        public string City { get; set; }
+        public string Town { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string Line { get; set; }
+    }
+}

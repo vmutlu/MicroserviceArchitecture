282d14a1c9010e95136fa6cf1558ea32b6888fc5
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-27 15:49:19 +0300

    baskedchangeevent classes added

---
+++ Shared/DotnetMicroserviceArchitecture.Core/Events/BasketChangeEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetMicroserviceArchitecture.Core.Events
+{
+    public class BasketChangeEvent
+    {
+        public string UserId { get; set; }
+        public string CourseId { get; set; }
+        public string UpdateName { get; set; }
+    }
+}

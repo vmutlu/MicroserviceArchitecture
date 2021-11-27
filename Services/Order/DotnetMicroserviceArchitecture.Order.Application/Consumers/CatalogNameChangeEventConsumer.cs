63e56b2ac05727958d35cee7886a4a119840f4f7
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-27 15:48:43 +0300

    catalog microservice eventual consistent classes created

---
+++ Services/Order/DotnetMicroserviceArchitecture.Order.Application/Consumers/CatalogNameChangeEventConsumer.cs
@@ -0,0 +1,28 @@
+﻿using DotnetMicroserviceArchitecture.Core.Events;
+using DotnetMicroserviceArchitecture.Order.Infrastructure.Context;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetMicroserviceArchitecture.Order.Application.Consumers
+{
+    public class CatalogNameChangeEventConsumer : IConsumer<CatalogNameChangeEvent>
+    {
+        private readonly OrderContext _orderContext;
+
+        public CatalogNameChangeEventConsumer(OrderContext orderContext) => (_orderContext) = (orderContext);
+
+        public async Task Consume(ConsumeContext<CatalogNameChangeEvent> context)
+        {
+            var orderItems = await _orderContext.OrderItems.Where(oi => oi.ProductId == context.Message.CatalogId).ToListAsync().ConfigureAwait(false);
+            orderItems.ForEach(u =>
+            {
+                u.UpdateOrderItem(context.Message.UpdatedName, u.URL, u.Price);
+            });
+
+            //catalog microservisinde ki data değişirse order microservisinde ki datayı da güncelle.
+            await _orderContext.SaveChangesAsync().ConfigureAwait(false);
+        }
+    }
+}
---
+++ Shared/DotnetMicroserviceArchitecture.Core/Events/CatalogNameChangeEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetMicroserviceArchitecture.Core.Events
+{
+    public class CatalogNameChangeEvent
+    {
+        public string CatalogId { get; set; }
+        public string UpdatedName { get; set; }
+    }
+}

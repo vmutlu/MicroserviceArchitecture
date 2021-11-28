db36a8fe36a5d7f9c5151e3529cf49f4f9ce7e15
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-28 10:08:15 +0300

    Identity microservice project for startup extension class writed and configured startup.cs

---
+++ IdentityServer/DotnetMicroserviceArchitecture.IdentityServer/Extensions/StartupExtensions.cs
@@ -0,0 +1,62 @@
+﻿using DotnetMicroserviceArchitecture.IdentityServer.Data;
+using DotnetMicroserviceArchitecture.IdentityServer.Models;
+using DotnetMicroserviceArchitecture.IdentityServer.Services;
+using IdentityServer4;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotnetMicroserviceArchitecture.IdentityServer.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddServices(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddLocalApiAuthentication();
+
+            services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
+                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                options.EmitStaticAudienceClaim = true;
+            })
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiResources(Config.ApiResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients)
+                .AddAspNetIdentity<ApplicationUser>();
+
+            builder.AddResourceOwnerValidator<DotnetMicroserviceArchitecture.IdentityServer.Services.PasswordValidator>();
+            builder.AddExtensionGrantValidator<TokenExchangeGrantValidator>();
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                    // register your IdentityServer with Google at https://console.developers.google.com
+                    // enable the Google+ API
+                    // set the redirect URI to https://localhost:5001/signin-google
+                    options.ClientId = "copy client ID from Google here";
+                    options.ClientSecret = "copy client secret from Google here";
+                });
+        }
+    }
+}
--- IdentityServer/DotnetMicroserviceArchitecture.IdentityServer/Startup.cs
+++ IdentityServer/DotnetMicroserviceArchitecture.IdentityServer/Startup.cs
@@ -3,6 +3,7 @@
 
 
 using DotnetMicroserviceArchitecture.IdentityServer.Data;
+using DotnetMicroserviceArchitecture.IdentityServer.Extensions;
 using DotnetMicroserviceArchitecture.IdentityServer.Models;
 using DotnetMicroserviceArchitecture.IdentityServer.Services;
 using IdentityServer4;
@@ -29,50 +30,7 @@ namespace DotnetMicroserviceArchitecture.IdentityServer
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddLocalApiAuthentication();
-
-            services.AddControllersWithViews();
-
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-            var builder = services.AddIdentityServer(options =>
-            {
-                options.Events.RaiseErrorEvents = true;
-                options.Events.RaiseInformationEvents = true;
-                options.Events.RaiseFailureEvents = true;
-                options.Events.RaiseSuccessEvents = true;
-
-                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
-                options.EmitStaticAudienceClaim = true;
-            })
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiResources(Config.ApiResources)
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients)
-                .AddAspNetIdentity<ApplicationUser>();
-
-            builder.AddResourceOwnerValidator<DotnetMicroserviceArchitecture.IdentityServer.Services.PasswordValidator>();
-            builder.AddExtensionGrantValidator<TokenExchangeGrantValidator>();
-
-            // not recommended for production - you need to store your key material somewhere secure
-            builder.AddDeveloperSigningCredential();
-
-            services.AddAuthentication()
-                .AddGoogle(options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-
-                    // register your IdentityServer with Google at https://console.developers.google.com
-                    // enable the Google+ API
-                    // set the redirect URI to https://localhost:5001/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
-                });
+            services.AddServices(Configuration);
         }
 
         public void Configure(IApplicationBuilder app)

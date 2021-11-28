a5fc03f761fc2ecf9c680181040fc7fcb6662b18
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-28 10:07:25 +0300

    Discount microservice project for startup extension class writed and configured startup.cs

---
+++ Services/Discount/DotnetMicroserviceArchitecture.DiscountAPI/Extensions/StartupExtensions.cs
@@ -0,0 +1,41 @@
+﻿using DotnetMicroserviceArchitecture.Core.Services.Abstract;
+using DotnetMicroserviceArchitecture.Core.Services.Concrete;
+using DotnetMicroserviceArchitecture.DiscountAPI.Services.Abstract;
+using DotnetMicroserviceArchitecture.DiscountAPI.Services.Concrete;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace DotnetMicroserviceArchitecture.DiscountAPI.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddServices(this IServiceCollection services, IConfiguration Configuration)
+        {
+            var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub"); //userId bilgisi taşıyan sub keywordunu mapleme
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.Authority = Configuration["IdentityServer"]; //token kontrolü
+                opt.Audience = "resource_discount";
+                opt.RequireHttpsMetadata = false;
+            });
+
+            services.AddHttpContextAccessor();
+
+            services.AddScoped<IIdentityService, IdentityService>();
+
+            services.AddScoped<IDiscountService, DiscountService>();
+
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy)); //controller üzerine Authorize attributune gerek yok
+            });
+        }
+    }
+}
--- Services/Discount/DotnetMicroserviceArchitecture.DiscountAPI/Startup.cs
+++ Services/Discount/DotnetMicroserviceArchitecture.DiscountAPI/Startup.cs
@@ -1,17 +1,10 @@
-using DotnetMicroserviceArchitecture.Core.Services.Abstract;
-using DotnetMicroserviceArchitecture.Core.Services.Concrete;
-using DotnetMicroserviceArchitecture.DiscountAPI.Services.Abstract;
-using DotnetMicroserviceArchitecture.DiscountAPI.Services.Concrete;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
+using DotnetMicroserviceArchitecture.DiscountAPI.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using System.IdentityModel.Tokens.Jwt;
 
 namespace DotnetMicroserviceArchitecture.DiscountAPI
 {
@@ -23,27 +16,7 @@ namespace DotnetMicroserviceArchitecture.DiscountAPI
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub"); //userId bilgisi taþýyan sub keywordunu mapleme
-
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
-            {
-                opt.Authority = Configuration["IdentityServer"]; //token kontrolü
-                opt.Audience = "resource_discount";
-                opt.RequireHttpsMetadata = false;
-            });
-
-            services.AddHttpContextAccessor();
-
-            services.AddScoped<IIdentityService, IdentityService>();
-
-            services.AddScoped<IDiscountService, DiscountService>();
-
-            services.AddControllers(opt =>
-            {
-                opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy)); //controller üzerine Authorize attributune gerek yok
-            });
+            services.AddServices(Configuration);
 
             services.AddSwaggerGen(c =>
             {

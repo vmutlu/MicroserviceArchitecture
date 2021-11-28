6e800dbb3ddd91aba0e65c70a90ac0914f6409a9
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-11-28 10:08:04 +0300

    Gateway project for startup extension class writed and configured startup.cs

---
+++ Gateways/DotnetMicroserviceArchitecture.Gateway/Extensions/StartupExtensions.cs
@@ -0,0 +1,24 @@
+﻿using DotnetMicroserviceArchitecture.Gateway.Handlers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+
+namespace DotnetMicroserviceArchitecture.Gateway.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddServices(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddHttpClient<TokenExchangeHandler>();
+
+            services.AddAuthentication().AddJwtBearer("GatewayScheme", options =>
+            {
+                options.Authority = Configuration["IdentityServer"]; //token kontrolü
+                options.Audience = "resource_gateway";
+                options.RequireHttpsMetadata = false;
+            });
+
+            services.AddOcelot().AddDelegatingHandler<TokenExchangeHandler>(); //TokenExchangeHandler nezaman devreye girsin ? config dosyasında belirtilecek
+        }
+    }
+}
--- Gateways/DotnetMicroserviceArchitecture.Gateway/Startup.cs
+++ Gateways/DotnetMicroserviceArchitecture.Gateway/Startup.cs
@@ -1,10 +1,9 @@
-using DotnetMicroserviceArchitecture.Gateway.Handlers;
+using DotnetMicroserviceArchitecture.Gateway.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 namespace DotnetMicroserviceArchitecture.Gateway
@@ -16,16 +15,7 @@ namespace DotnetMicroserviceArchitecture.Gateway
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient<TokenExchangeHandler>();
-
-            services.AddAuthentication().AddJwtBearer("GatewayScheme", options =>
-            {
-                options.Authority = Configuration["IdentityServer"]; //token kontrolü
-                options.Audience = "resource_gateway";
-                options.RequireHttpsMetadata = false;
-            });
-
-            services.AddOcelot().AddDelegatingHandler<TokenExchangeHandler>(); //TokenExchangeHandler nezaman devreye girsin ? config dosyasýnda belirtilecek
+            services.AddServices(Configuration);
         }
 
         async public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
